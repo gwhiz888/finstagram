a0ef08ffe02cfd50e0d3dbf2d5bdd76dce059000
gwhiz888 <accbyte.digital@gmail.com>
2022-02-01 04:42:22 +0000

    ruby interacting with sinatra

--- app/actions.rb
+++ app/actions.rb
@@ -14,66 +14,49 @@ end
 
 get '/' do
 
-     finstagram_post_shark = {
-          username: "sharky_j",
-          avatar_url: "http://naser4ca.com/images/sharky_j.jpg",
-          photo_url: "http://naserca.com/images/shark.jpg",
+     @finstagram_post_shark = {
+         username: "sharky_j",
+         avatar_url: "http://naserca.com/images/sharky_j.jpg",
+       photo_url: "http://naserca.com/images/shark.jpg",
           humanized_time_ago:  humanized_time_ago(15),
           like_count: 0,
           comment_count: 1,
-          comments:  [{
-               username: "shark_j",
+         comments:  [{
+              username: "shark_j",
                text: "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
           }]
           }
      
-     finstagram_post_whale = {
+     @finstagram_post_whale = {
                username: "kirk_whalum",
-               avatar_url: "http://naser4ca.com/images/kirk_whalum.jpg",
+              avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
                photo_url: "http://naserca.com/images/whale.jpg",
-               humanized_time_ago:  humanized_time_ago(65),
-               like_count: 0,
-               comment_count: 1,
-               comments:  [{
-                    username: "kirk_whalum",
-                    text: "#weekendvibes"
-                    }]
-               }
+              humanized_time_ago:  humanized_time_ago(65),
+              like_count: 0,
+              comment_count: 1,
+              comments:  [{
+                   username: "kirk_whalum",
+                   text: "#weekendvibes"
+                   }]
+              }
      
-               finstagram_post_marlin = {
-                    username: "marlin_peppa",
-                    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-                    photo_url: "http://naserca.com/images/marlin.jpg",
-                    humanized_time_ago: humanized_time_ago(190),
-                    like_count: 0,
-                    comment_count: 1,
-                    comments: [{
-                      username: "marlin_peppa",
-                      text: "lunchtime! ;)"
-                    }]
-                  }
+               
+               @finstagram_post_marlin = {
+                  username: "marlin_peppa",
+                   avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+                   photo_url: "http://naserca.com/images/marlin.jpg",
+                   humanized_time_ago: humanized_time_ago(190),
+                   like_count: 0,
+                   comment_count: 1,
+                   comments: [{
+                     username: "marlin_peppa",
+                     text: "lunchtime! ;)"
+                  }]
+                }
      
 
-     [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+     @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
 
+     erb(:index)
 end
 
-     
-
-
-     # if the time_ago_in minutes is greater than 60
-     #if time_ago_in_minutes >= 60
-
-          # return this string
-          #"#{time_ago_in_minutes / 60} hours ago"
-
-     #elsif time_ago_in_minutes == 60
-          #"an hour"
-
-     # if it's less than or equal
-     #else 
-
-          # return this instead
-          #"#{time_ago_in_minutes} minutes ago"
-     #end
-#end
