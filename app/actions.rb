83f3bc447e72d54ca1880ce8416ee8c13dcf3d69
gwhiz888 <accbyte.digital@gmail.com>
2022-02-15 04:48:14 +0000

    added login and modified actions

--- app/actions.rb
+++ app/actions.rb
@@ -3,11 +3,16 @@
      #File.read(File.join('app/views', 'index.html'))
 #end
 
+helpers do 
+     def current_user
+          User.find_by(id: session[:user_id])
+     end
+end
 
 get '/' do
 
      @finstagram_posts = FinstagramPost.order(created_at: :desc)
-
+     
      erb(:index)
 end
 
@@ -17,6 +22,17 @@ get '/signup' do #if a user navigates to the path "/signup",
 
      erb(:signup) # render "app/views/signup.erb"
 
+end
+
+get '/login' do #if a user navigates to the path "/login",
+   
+     erb(:login) # render "app/views/login.erb
+     
+end
+
+get '/logout' do
+     session[:user_id] = nil 
+     "Logout successful!"
 end
 
 post '/signup' do  
@@ -33,14 +49,36 @@ post '/signup' do
           
      # if user validations pass and user is saved
      if @user.save
-          "User #{@username} created!"
-    
+          redirect to('/login')
+        
      else
      
           erb(:signup)
        
     end
 end
+post '/login' do # when we submit a form with an action of /login
+     username = params[:username]
+     password = params[:password]
+
+     # 1. find user by username 
+     @user = User.find_by(username: username)
+
+
+          # check if that user's password matches the password input 
+          #3. if the passwords match 
+          if @user && @user.authenticate(password)
+               session[:user_id] = @user.id
+               redirect to('/')
+
+          else
+               @error_message = "Login failed."
+               erb(:login)
+          end
+
+end
 
 # post / login route
 #    if user && user.authenticate(password)
+          #session[:user_id] = user.id 
+
